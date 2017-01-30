4c53d4591762367302b8aa3dc9a92f200e790a83
Gary Lee Sundquist Jr <gary.sundquist1@gmail.com>
2017-01-29 21:03:47 -0800

    Add files via upload

---
+++ LocaleChangeListener.java
@@ -0,0 +1,21 @@
+/**
+ * Gary Sundquist
+ * Code from tutorial at: https://www.tutorialspoint.com/jsf/jsf_valuechangelistener_tag.htm
+ */
+package com.tutorialspoint.test;
+import javax.faces.context.FacesContext;
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ValueChangeEvent;
+import javax.faces.event.ValueChangeListener;
+
+public class LocaleChangeListener implements ValueChangeListener {
+   @Override
+   public void processValueChange(ValueChangeEvent event)
+      throws AbortProcessingException {
+      //access country bean directly
+      UserData userData = (UserData) FacesContext.getCurrentInstance().
+         getExternalContext().getSessionMap().get("userData");
+
+      userData.setSelectedCountry(event.getNewValue().toString());
+   }
+}
